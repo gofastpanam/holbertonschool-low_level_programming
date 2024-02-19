07f2cb0d9ba24fea158995c63724ed788d2b765c
gofastpanam <8972@holbertonstudents.com>
2024-02-20 00:04:56 +0100

    6

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,12 +1,12 @@
 /**
  * put with proper grammar, but the outcome is a piece of art,
- * use printf and not the function puts 
+ * use printf and not the function puts
  * end with the value 0
  */
 
 #include <stdio.h>
 int main(void)
 {
-printf ("with proper grammar, but the outcome is a piece of art,\n");
+printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
 }
