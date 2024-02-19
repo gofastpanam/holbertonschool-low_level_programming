1f315e4b5588cdfad147a52c1c03d9f7a26c300f
gofastpanam <8972@holbertonstudents.com>
2024-02-19 22:05:01 +0100

    4

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,8 +1,12 @@
+/**
+ * put Programming is like building a multilingual puzzle
+ * use the function puts and not printf
+ * end with the value 0
+ */
+
 #include <stdio.h>
 int main(void)
-/* Write a C program that prints words */
 {
 puts("\"Programming is like building a multilingual puzzle");
-// your program should end with the value zero
 return (0);
 }
