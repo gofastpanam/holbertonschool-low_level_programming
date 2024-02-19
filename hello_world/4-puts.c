6c5b95f761681089ec1fc7c854dc9789e2796655
gofastpanam <8972@holbertonstudents.com>
2024-02-19 15:34:38 +0100

    4

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,4 +1,12 @@
 #include <stdio.h>
+
+/*
+*Write a C program that prints text.
+*Use the function puts
+*You are not allowed to use printf
+*Your program should end with the value 0
+*/
+
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
