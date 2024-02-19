345b54f2b0d5e4eeec536907d5e73e0d325b4977
gofastpanam <8972@holbertonstudents.com>
2024-02-20 00:17:40 +0100

    5

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,10 +1,10 @@
+#include <stdio.h>
+
 /**
  * put Programming is like building a multilingual puzzle
  * use the function puts and not printf
  * end with the value 0
  */
-
-#include <stdio.h>
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,10 +1,10 @@
+#include <stdio.h>
+
 /**
  * put with proper grammar, but the outcome is a piece of art,
  * use printf and not the function puts
  * end with the value 0
  */
-
-#include <stdio.h>
 int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
