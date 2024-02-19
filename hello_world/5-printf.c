67e6a76fd866ff0257875fab9ba710a1377d2038
gofastpanam <8972@holbertonstudents.com>
2024-02-19 22:18:52 +0100

    5

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,4 +1,8 @@
-/* Use the function printf */
+/**
+ * put with proper grammar, but the outcome is a piece of art,
+ * use printf and not the function puts 
+ * end with the value 0
+ */
 
 #include <stdio.h>
 int main(void)
