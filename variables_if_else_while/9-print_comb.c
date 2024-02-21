292478d2cb0df6f70a5ced0c79ac3ca6088723d5
gofastpanam <8972@holbertonstudents.com>
2024-02-21 16:55:39 +0100

    .

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+* main - prints the alphabet in lowercase
+* Prints all possible combinations of single-digit numbers.
+* Return: 0 (Success)
+*/
+int main(void)
+{
+int c;
+for (c = 0; c < 10; ++c)
+{
+printf("%d,", c);
+}
+printf("\n");
+return (0);
+}
