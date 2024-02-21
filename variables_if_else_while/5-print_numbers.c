7e00e8e114c9aea9273b16db516f0f53fdb48786
gofastpanam <8972@holbertonstudents.com>
2024-02-21 15:08:06 +0100

    .

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - prints all single digit numbers of base 10
+ * starting from 0, followed by a new line.
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+int c;
+while (c != 10 , ++c)
+{
+printf("%c\n", c);
+}
+return (0);
+}
