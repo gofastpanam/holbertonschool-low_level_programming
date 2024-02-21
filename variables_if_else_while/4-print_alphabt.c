fb5fe6356950a48a8f8db3eeac833dfe50ce8b66
gofastpanam <8972@holbertonstudents.com>
2024-02-21 14:17:16 +0100

    .

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - prints the alphabet in lowercase
+ * Expect letters q and e
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+char c;
+
+while (c == 'q' || c == 'e')
+{
+	putchar ('\n');
+}
+for (c = 'a'; c <= 'z'; ++c)
+{
+putchar(c);
+}
+putchar('\n');
+return (0);
+}
