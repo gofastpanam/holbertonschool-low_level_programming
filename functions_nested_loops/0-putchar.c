5f9d3e5d5c6cf769f6935d058eaa0aa6ed3f092e
gofastpanam <8972@holbertonstudents.com>
2024-02-21 20:16:08 +0100

    .

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+* main - prints the alphabet in lowercase.
+* prints _putchar, followed by a new line.
+* Return: 0 (Success).
+*/
+int main(void)
+{
+putchar("_putchar\n");
+return (0);
+}
