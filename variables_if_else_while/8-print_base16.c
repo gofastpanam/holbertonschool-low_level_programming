5998c56fd751dd31ecb646736b84255bf1aeb078
gofastpanam <8972@holbertonstudents.com>
2024-02-21 16:44:55 +0100

    .

--- variables_if_else_while/8-print_base16.c
+++ variables_if_else_while/8-print_base16.c
@@ -1,18 +1,22 @@
 #include <stdio.h>
+
 /**
- * main - prints all single digit numbers of base 10
- * starting from 0 and followed by a new line.
+ * main - prints the alphabet in lowercase
+ *
  * Return: 0 (Success)
  */
 int main(void)
 {
-int c;
+char c;
 
 for (c = 48; c <= 57 ; ++c)
 {
 putchar(c);
 }
-
+for (c = 64; c <= 70 ; ++c)
+{
+putchar(c);
+}
 putchar('\n');
 return (0);
 }
