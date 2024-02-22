6c180e161a26f302449eb28ac8ea39c2c184a076
gofastpanam <8972@holbertonstudents.com>
2024-02-22 12:06:25 +0100

    .

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+* main - prints alphabet,
+* followed by a new line,
+* Return: 0 (Success).
+*/
+
+void print_alphabet(void)
+{
+
+char i;
+for (i = 'a'; i <= 'z'; i++)
+{
+_putchar(i);
+}
+_putchar('\n');
+return;
+
+}
