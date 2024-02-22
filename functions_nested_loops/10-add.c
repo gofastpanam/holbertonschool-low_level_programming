dc74f3a18ec489215f4b2c660c91c012665f908d
gofastpanam <8972@holbertonstudents.com>
2024-02-22 19:15:03 +0100

    .

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,9 @@
+#include "main.h"
+
+/**
+* int add - a function that adds two integers
+* and returns the result.
+* main - entry point
+* return: result
+*/
+int add(int, int);
--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,18 +1,30 @@
 #include "main.h"
 
 /**
+* print_alphabet_x10 - function to print aplhabet 10 times
+* @void - no values in enter
 * main - entry
 * print alphabet
 * Return: 0 (Success).
 */
 
-void print_alphabet(void)
+void print_alphabet_x10(void)
 {
-char i;
-for (i = 'a'; i <= 'z'; i++)
-{
-_putchar(i);
+    int i;
+    char letter;
+
+    for (i = 0; i < 10; i++)
+    {
+        letter = 'a';
+        while (letter <= 'z')
+        {
+            _putchar(letter);
+			letter++;
+			return;
+
+        }
+        _putchar('\n');
+		return (0);
+    }
 }
-_putchar('\n');
-return;
-}
+
