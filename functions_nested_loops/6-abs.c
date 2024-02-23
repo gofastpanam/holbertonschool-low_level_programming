86936ade28e36fe94c84eb4edc0a620ef302472e
gofastpanam <8972@holbertonstudents.com>
2024-02-23 11:48:20 +0100

    .

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+* _abs - The function that computes the absolute value of an integrer.
+* main - Check the code
+* Return:
+*
+*/
+int _abs(int)
+{
+	int j;
+
+	if (j < 0)
+	{
+		j = -j;
+	}
+	else
+	{
+		j = j;
+	}
+}
---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+* print_last_digit - The function that prints the last digit of a number
+* Return: The value of the last digit
+*
+*
+*/
+int print_last_digit(int num)
+{
+	int j;
+	j = num % 10;
+
+	return (j);
+}
