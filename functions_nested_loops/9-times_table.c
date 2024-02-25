8985d07f7bb026cad7ef1c3a0c5601e5e11706b5
gofastpanam <8972@holbertonstudents.com>
2024-02-25 16:06:19 +0100

    .

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -1,9 +1,44 @@
 #include "main.h"
 
 /**
-* times_table - prints the 9 times table, starting with 0.
-*
-*/
+ * times_table - Prints the 9 times table
+ */
+void times_table(void)
+{
+    int i, j, result;
 
-void times_table(void);
+    for (i = 0; i <= 9; i++)
+    {
+        for (j = 0; j <= 9; j++)
+        {
+            result = i * j;
 
+            if (result < 10 && j != 0)
+            {
+                _putchar(' ');
+                _putchar(' ');
+            }
+            else if (result >= 10)
+            {
+                _putchar(' ');
+            }
+
+            if (result < 10)
+            {
+                _putchar(result + '0');
+            }
+            else
+            {
+                _putchar((result / 10) + '0');
+                _putchar((result % 10) + '0');
+            }
+
+            if (j < 9)
+            {
+                _putchar(',');
+                _putchar(' ');
+            }
+        }
+        _putchar('\n');
+    }
+}
