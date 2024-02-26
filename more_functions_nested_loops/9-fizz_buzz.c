3d556bdf748b63e6d0e0e9f92409be6d920235fa
gofastpanam <8972@holbertonstudents.com>
2024-02-26 22:37:03 +0100

    .

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -1,10 +1,13 @@
-#include <stdio.h>
+#include "main.h"
 
+/**
+ * fizz_buzz - prints Fizz-Buzz sequence up to 100
+ */
 int main(void)
 {
-	int i;
+    int i;
 
-    for (i = 0; i < 100; i++)
+    for (i = 1; i <= 100; i++)
     {
         if (i % 3 == 0 && i % 5 == 0)
             printf("FizzBuzz ");
@@ -15,5 +18,6 @@ int main(void)
         else
             printf("%d ", i);
     }
-    return 0;
+    printf("\n");
+	return (0);
 }
