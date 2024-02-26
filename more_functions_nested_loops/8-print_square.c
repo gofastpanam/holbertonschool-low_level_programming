9f6e1f17a263e60f8acb1abbcb4b75a571a44e00
gofastpanam <8972@holbertonstudents.com>
2024-02-26 14:16:30 +0100

    .

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+* print_square - Prints a square.
+* @size: the size of the square.
+*
+*/
+void print_square(int size)
+{
+	if (size > 0)
+	{
+		int i;
+
+		for (i = 0; i <= size; i++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+
+	}
+	else
+	{
+		_putchar('\n');
+	}
+
+}
