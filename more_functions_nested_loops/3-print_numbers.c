fbbb0be6d4c240be71ec0de24ed4ff7d62d40d91
gofastpanam <8972@holbertonstudents.com>
2024-02-26 12:12:31 +0100

    .

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -2,13 +2,13 @@
 
 /**
 * print_numbers - Prints the numbers, from 0 to 9, followed by a new line.
-* Return: 0.
+* @c: number to print.
 *
 */
 void print_numbers(void)
 {
 	int c;
-	for (c = 0; c <= 9; c++)
+	for (c = '0'; c <= '9'; c++)
 	{
 	_putchar(c);
 	}
