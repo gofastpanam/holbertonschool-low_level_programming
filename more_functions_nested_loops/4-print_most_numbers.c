0ffc7914122b73899d3f5267b7e058e6c63b4a58
gofastpanam <8972@holbertonstudents.com>
2024-02-26 12:25:03 +0100

    .

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -2,15 +2,20 @@
 
 /**
 * print_numbers - Prints the numbers, from 0 to 9, followed by a new line.
+* not print 2 and 4.
 * @c: Number to print from 0 to 9.
 *
 */
-void print_numbers(void)
+void print_most_numbers(void)
 {
 	int c;
 	for (c = '0'; c <= '9'; c++)
 	{
-	_putchar(c);
+		if (c != '2' && c != '4')
+		{
+			_putchar(c);
+		}
 	}
 	_putchar('\n');
 }
+
