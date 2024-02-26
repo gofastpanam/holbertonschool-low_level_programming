3735a38883bf9f9d25d2cf1f738fc71cc76ee7b1
gofastpanam <8972@holbertonstudents.com>
2024-02-26 15:01:47 +0100

    .

--- more_functions_nested_loops/6-print_line.c
+++ more_functions_nested_loops/6-print_line.c
@@ -7,13 +7,16 @@
 */
 void print_line(int n)
 {
+	if (n > 0)
+	{
 		int i;
-		for (i = 0; i < n; i++)
+		for (i = 0; i <= n; i++)
 		{
 			_putchar('_');
 			_putchar('\n');
 		}
 
+	}
+	else
 	_putchar('\n');
-
 }
