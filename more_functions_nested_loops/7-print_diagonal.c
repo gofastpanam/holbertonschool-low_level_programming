4ac14bc2248a43dc4b16262ed4a8a21b2cec9094
gofastpanam <8972@holbertonstudents.com>
2024-02-26 13:58:39 +0100

    .

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -7,14 +7,19 @@
 */
 void print_diagonal(int n)
 {
-		int i;
-		for (i = 0; i <= n; i++)
+
+		if (n <= 0)
+		{
+			_putchar('\n');
+		}
+		else
+		{
+			int i;
+
+			for (i = 0; i < n; i++)
 		{
 			_putchar('\\');
 			_putchar('\n');
 		}
-		if (i < 0)
-		{
-			_putchar('\n');
 		}
 }
