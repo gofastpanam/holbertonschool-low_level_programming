02a22cd4aebc5f546adf50f5be2ba101658a825f
gofastpanam <8972@holbertonstudents.com>
2024-02-26 22:52:58 +0100

    .

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -1,28 +1,38 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
- * fizz_buzz - prints Fizz-Buzz sequence up to 100
+ * main - Prints Fizz-Buzz sequence up to 100
  *
+ * Return: Always 0
  */
-int main() {
+int main(void)
+{
+    int i;
 
-	int i;
-	
-    for (i = 0; i < 100; i++) {
-        if (i % 3 == 0 && i % 5 == 0) {
+    for (i = 1; i <= 100; i++)
+    {
+        if (i % 3 == 0 && i % 5 == 0)
+        {
             printf("FizzBuzz");
-        } else if (i % 3 == 0) {
+        }
+        else if (i % 3 == 0)
+        {
             printf("Fizz");
-        } else if (i % 5 == 0) {
+        }
+        else if (i % 5 == 0)
+        {
             printf("Buzz");
-        } else {
+        }
+        else
+        {
             printf("%d", i);
         }
-
-        if (i <= 100) {
+        if (i < 100)
+        {
             printf(" ");
         }
     }
     printf("\n");
-    return 0;
+    return (0);
 }
