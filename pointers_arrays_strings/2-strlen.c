1f3a2a16c7fb669cb823735c368d6c668245e08f
gofastpanam <8972@holbertonstudents.com>
2024-02-28 09:33:32 +0100

    .

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _strlen - Returns the length of a string.
+ * @s: The string to be measured.
+ *
+ * Return: The length of the string.
+ */
+int _strlen(char *s)
+{
+    int length;
+
+    for (length = 0; *s > '\0'; length++)
+	{
+	}
+	return (length);
+}
