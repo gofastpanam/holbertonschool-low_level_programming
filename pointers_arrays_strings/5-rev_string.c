9312b33311c4f36e29817ef6de09fcb05e5098b3
gofastpanam <8972@holbertonstudents.com>
2024-02-29 14:05:03 +0100

    .

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+* rev_string - Reverses a string.
+* @s: the string.
+*
+*/
+void rev_string(char *s)
+{
+	int rev;
+	int i;
+
+	while (s[rev] != '\0')
+	rev++;
+	for (i = s[rev] - 1; i != 0)
+}
