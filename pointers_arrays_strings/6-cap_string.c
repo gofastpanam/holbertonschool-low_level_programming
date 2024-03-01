a37462a995fe2490fa1bf5d8fabab8eda08cde22
gofastpanam <8972@holbertonstudents.com>
2024-03-01 21:59:17 +0100

    .

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -2,10 +2,17 @@
 
 /**
 * cap_string - Capitalizes all words of a string.
-*
+* @s: the strin
 *
 */
-char *cap_string(char *);
+char *cap_string(char *s)
 {
+	int i;
 
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (i == ',' &&  i == ';' &&  i =='.' && i == '!' && i == '?' && i == '"' && i == ')' && i  == '{' && i == '}')
+		s[i] = s[i] - 32;
+	}
+	return (s);
 }
