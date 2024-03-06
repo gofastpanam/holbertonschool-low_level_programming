22a4ed78c9136f822a908ca605c9e0ae245b5ae3
gofastpanam <8972@holbertonstudents.com>
2024-03-06 19:20:05 +0100

    .

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -7,11 +7,11 @@
 */
 void _puts_recursion(char *s)
 {
-	int i;
-
-	for (i = 0; s[i] != '\0'; i++)
+	if (*s == '\0')
 	{
-		_putchar(s[i]);
+		_putchar('\n');
+		return;
 	}
-	_putchar('\n');
+	_putchar(*s);
+	_puts_recursion(s + 1);
 }
