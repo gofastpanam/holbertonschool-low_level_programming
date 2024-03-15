3bb6127ce843daeba2710df9fff0abd6ad8eca05
gofastpanam <8972@holbertonstudents.com>
2024-03-15 16:50:08 +0100

    .

---
+++ structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+* struct dog - structure
+* @name: member
+* @age: member
+* @owner: member
+*
+*/
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
+#endif/* DOG_H */
