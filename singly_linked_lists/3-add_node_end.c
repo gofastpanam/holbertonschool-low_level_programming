8b9b3a90366679cf3134203445181e4bc5970690
gofastpanam <8972@holbertonstudents.com>
2024-04-04 14:09:25 +0200

    .

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -13,22 +13,29 @@ list_t *add_node_end(list_t **head, const char *str)
 {
 	list_t *new_node;
 	list_t *last_node = *head;
+	int len = 0;
+	int i;
 
 	if (str == NULL)
 	return (NULL);
+
+	while (str[len] != '\0')
+	len++;
 
 	new_node = malloc(sizeof(list_t));
 	if (new_node == NULL)
 	return (NULL);
 
-	new_node->str = strdup(str);
-	if (new_node->str == NULL)
+	new_node->str = malloc((len + 1) * sizeof(char));
+    if (new_node->str == NULL)
 	{
 		free(new_node);
 		return (NULL);
 	}
 
-	new_node->len = strlen(str);
+	for (i = 0; i <= len; i++)
+	new_node->str[i] = str[i];
+	new_node->len = len;
 	new_node->next = NULL;
 
 	if (*head == NULL)
@@ -39,8 +46,6 @@ list_t *add_node_end(list_t **head, const char *str)
 
 	while (last_node->next != NULL)
 	last_node = last_node->next;
-
 	last_node->next = new_node;
-
 	return (new_node);
 }
