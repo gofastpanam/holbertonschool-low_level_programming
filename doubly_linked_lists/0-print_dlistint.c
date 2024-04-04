83d46b3e69d7c35b8d23eda85c424442905108db
gofastpanam <8972@holbertonstudents.com>
2024-04-04 15:46:56 +0200

    .

--- doubly_linked_lists/0-print_dlistint.c
+++ doubly_linked_lists/0-print_dlistint.c
@@ -4,11 +4,12 @@
 /**
 * print_dlistint - prints all the elements of a dlistint_t list
 * @h: elements to print
-*
+* Return: numbers of nodes
 */
 size_t print_dlistint(const dlistint_t *h)
 {
 	size_t count = 0;
+
 	while (h)
 	{
 		printf("%d\n", h->n);
